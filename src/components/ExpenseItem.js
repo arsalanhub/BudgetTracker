7a893aeaf614798515e34a8caecc81b40d5cd186
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-07-30 16:08:13 +0530

    variables added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
+  const expenseDate = new Date(2022, 7, 30);
+  const expenseName = "Car Insurance";
+  const expensePrice = 999;
   return (
     <div className="expense-item">
-        <div>July 30, 2022</div>
-        <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">$999</div>
-        </div>
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseName}</h2>
+        <div className="expense-item__price">${expensePrice}</div>
+      </div>
     </div>
   );
 }
