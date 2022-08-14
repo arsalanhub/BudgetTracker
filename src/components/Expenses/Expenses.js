fff12f41d332cf522a0a2b94f58ceb87da4a084a
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-08-14 13:29:57 +0530

    conditions added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,10 +9,24 @@ const Expenses = (props) => {
   const [filteredYear, setfilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
     setfilteredYear(selectedYear);
-    setList(props.item.filter((expense) => {
-      return String(expense.date.getFullYear())===selectedYear;
-    })) 
+    setList(
+      props.item.filter((expense) => {
+        return String(expense.date.getFullYear()) === selectedYear;
+      })
+    );
   };
+
+  let expensesContent = <p>No Content Found for selected year.</p>;
+  if (list.length > 0) {
+    expensesContent = list.map((expense) => (
+      <ExpenseItem
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        key={expense.id}
+      />
+    ));
+  }
   return (
     <div>
       <Card className="expenses">
@@ -20,14 +34,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {list.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-            key={expense.id}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
