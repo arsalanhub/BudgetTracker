258ca41613fd9470fa78cd343c2c86cec3ca6e7c
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-08-14 12:29:08 +0530

    appending to list using useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  { id: "e1", title: "Book", amount: 60, date: new Date(2022, 2, 24) },
+  { id: "e2", title: "Pencil", amount: 10, date: new Date(2022, 7, 12) },
+  { id: "e3", title: "Pizza", amount: 200, date: new Date(2022, 1, 1) },
+  { id: "e4", title: "Juice", amount: 30, date: new Date(2022, 4, 14) },
+];
+
 const App = () => {
-  const expenses = [
-    { id: "e1", title: "Book", amount: 60, date: new Date(2022, 2, 24) },
-    { id: "e2", title: "Pencil", amount: 10, date: new Date(2022, 7, 12) },
-    { id: "e3", title: "Pizza", amount: 200, date: new Date(2022, 1, 1) },
-    { id: "e4", title: "Juice", amount: 30, date: new Date(2022, 4, 14) },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpensehandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpensehandler} />
       <Expenses item={expenses} />
     </div>
   );
