fc36c372d9c1008e5399393edd8256d2911b15a2
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-11-22 11:58:39 +0530

    logout

--- client/src/pages/BudgetDashboard.js
+++ client/src/pages/BudgetDashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Expenses from "../components/Expenses/Expenses";
 import NewExpense from "../components/NewExpense/NewExpense";
 import styles from "./BudgetDashboard.module.css";
+import { useNavigate } from "react-router-dom";
 
 const DUMMY_EXPENSES = [
   { id: "e1", title: "Book", amount: 60, date: new Date(2022, 2, 24) },
@@ -12,21 +13,33 @@ const DUMMY_EXPENSES = [
 
 const BudgetDashboard = () => {
   const [username, setUsername] = useState("");
-  useEffect(() => {
-    setUsername(JSON.parse(localStorage.getItem("user")).username);
-  });
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let user = localStorage.getItem("user");
+    if (user == null) return navigate("/");
+    user = JSON.parse(user);
+    setUsername(user.username);
+  }, []);
 
   const addExpensehandler = (expense) => {
     setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
   };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  };
   return (
     <>
       <nav className={styles.navClass}>
         <a>Welcome, {username}</a>
-        <button className={styles.buttonClass}>Logout</button>
+        <button className={styles.buttonClass} onClick={logoutHandler}>
+          Logout
+        </button>
       </nav>
       <div>
         <NewExpense onAddExpense={addExpensehandler} />
