7b19ec1a8370f92154a3c5bc076427c6c27729e3
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-11-23 22:18:08 +0530

    date functionality

--- client/src/components/Expenses/ExpenseDate.js
+++ client/src/components/Expenses/ExpenseDate.js
@@ -1,25 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
-  const monthPicker = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  let date = props.date.split("-");
-  let year = date[0];
-  let month = monthPicker[parseInt(date[1]) - 1];
-  let day = date[2][0] + date[2][1];
+  let date = new Date(props.date);
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
