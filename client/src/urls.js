74a1ca816800d4e832f34294892e9717564d7f90
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-11-27 14:36:01 +0530

    API branch

--- client/src/urls.js
+++
@@ -1,8 +0,0 @@
-export const baseURL = "http://localhost:5000";
-
-export const loginURL = `${baseURL}/login`;
-export const registerURL = `${baseURL}/register`;
-export const AddExpenseURL = `${baseURL}/AddExpense`;
-export const GetExpenseURL = `${baseURL}/GetExpense`;
-export const getYearURL = `${baseURL}/GetYear`;
-export const filterYearURL = `${baseURL}/FilterYear`
